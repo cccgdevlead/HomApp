428c0db039c1733c7cf97d232bc44524a3a94e55
Yemi Ilupeju DA <Yemi.Ilupeju.DA@LIQNA.LOCAL>
2025-03-17 15:46:50 -0600

    Remove sensitive information

---
+++ src/app/series.service.ts
@@ -0,0 +1,53 @@
+// series.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, from } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { S3Client, ListObjectsV2Command } from '@aws-sdk/client-s3';
+
+export interface AudioFile {
+  url: string;
+  title: string;
+  duration: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeriesService {
+  private readonly s3Client: S3Client;
+  private readonly bucketName = 'elasticbeanstalk-us-east-2-058264386413';
+  private readonly region = 'us-east-2';
+
+  constructor(private http: HttpClient) {
+    this.s3Client = new S3Client({ 
+      region: this.region,
+      credentials: {
+      }
+    });
+  }
+
+  getSeries(): Observable<any> {
+    return this.http.get(`https://${this.bucketName}.s3.${this.region}.amazonaws.com/series.json`);
+  }
+
+  getSeriesAudioFiles(seriesName: string): Observable<AudioFile[]> {
+    const command = new ListObjectsV2Command({
+      Bucket: this.bucketName,
+      Prefix: `${seriesName}/`,
+      MaxKeys: 100
+    });
+
+    return from(this.s3Client.send(command)).pipe(
+      map(response => {
+        return (response.Contents || [])
+          .filter(item => item.Key?.endsWith('.mp3'))
+          .map(item => ({
+            url: `https://${this.bucketName}.s3.${this.region}.amazonaws.com/${item.Key}`,
+            title: item.Key?.split('/').pop()?.replace('.mp3', '') || '',
+            duration: '3:00'
+          }));
+      })
+    );
+  }
+}
